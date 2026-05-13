d5bb91a8efcb09174faab032b0220aa9a05c8a81
BloatPlug <ernests1012@gmail.com>
2026-05-13 20:07:39 +0300

    Test

--- src/test/java/com/NoTrollsInParty/NoTrollsInPartyTest.java
+++ src/test/java/com/NoTrollsInParty/NoTrollsInPartyTest.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.NoTrollsInParty;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class NoTrollsInPartyTest
 {
 	public static void main(String[] args) throws Exception
 	{
